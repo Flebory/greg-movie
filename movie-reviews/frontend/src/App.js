53605b0e721fd688fe24224a3f8c055d738cd962
Flebory <123499686+Flebory@users.noreply.github.com>
2024-02-28 10:10:10 -0500

    Create Navigation Header Bar

--- movie-reviews/frontend/src/App.js
+++ movie-reviews/frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Switch, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+import AddReview from "./components/add-review"
+import MoviesList from "./components/movies-list"
+import Movie from "./components/movie"
+import Login from "./components/login"
 
 function App() {
+  const [user, setUser] = React.useState(null)
+
+  async function login(user = null){
+    setUser(user)
+  }
+
+  async function logout(){
+    setUser(null)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="1g">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              { user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
